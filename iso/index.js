c5e84913e26a7d1e4cbb2d2fc39fcbc96bef4c31
kalin.krustev <kalin.krustev@softwaregroup-bg.com>
2017-01-27 15:55:23 +0200

    fix: improve ISO

--- iso/index.js
+++ iso/index.js
@@ -19,32 +19,55 @@ function getBalance(field, balanceType) {
 }
 
 module.exports = {
-    toISO: function(version, msg) {
-        if (version === 0) {
-            return {
-                mtid: '0200',
-                '2': msg.udfAcquirer && msg.udfAcquirer.card,
-                '3': msg.udfAcquirer && msg.udfAcquirer.processingCode,
-                '4': msg.amount && msg.amount.transfer && msg.amount.transfer.cents,
-                '7': msg.localDateTime && msg.localDateTime.slice(-10),
-                '12': msg.localDateTime && msg.localDateTime.slice(-6),
-                '13': msg.localDateTime && msg.localDateTime.slice(-10).substring(0, 4),
-                '15': msg.destinationSettlementDate && msg.destinationSettlementDate.slice(-10).substring(0, 4),
-                '18': msg.udfAcquirer && msg.udfAcquirer.merchantType,
-                '32': msg.udfAcquirer && msg.udfAcquirer.institutionCode,
-                '35': msg.udfAcquirer && msg.udfAcquirer.track2,
-                '37': msg.transferId,
-                '41': msg.udfAcquirer && msg.udfAcquirer.terminalId,
-                '42': msg.udfAcquirer && msg.udfAcquirer.identificationCode,
-                '43': msg.udfAcquirer && msg.udfAcquirer.terminalName,
-                '49': currency.numeric(msg.transferCurrency),
-                '52': msg.pinBlock,
-                '102': msg.sourceAccount,
-                '103': msg.destinationAccount,
-                '122': msg.merchantId,
-                '123': msg.merchantInvoice
-            };
+    toISO: function(version, {udfAcquirer, amount, localDateTime, destinationSettlementDate, transferId, transferCurrency, pinBlock, sourceAccount,
+        destinationAccount, merchantId, merchantInvoice}) {
+        function base() {
+            if (version === 0) {
+                return {
+                    '2': udfAcquirer && udfAcquirer.card,
+                    '3': udfAcquirer && udfAcquirer.processingCode,
+                    '4': amount && amount.transfer && amount.transfer.cents,
+                    '7': localDateTime && localDateTime.slice(-10),
+                    '12': localDateTime && localDateTime.slice(-6),
+                    '13': localDateTime && localDateTime.slice(-10).substring(0, 4),
+                    '15': destinationSettlementDate && destinationSettlementDate.slice(-10).substring(0, 4),
+                    '18': udfAcquirer && udfAcquirer.merchantType,
+                    '32': udfAcquirer && udfAcquirer.institutionCode,
+                    '35': udfAcquirer && udfAcquirer.track2,
+                    '37': transferId,
+                    '41': udfAcquirer && udfAcquirer.terminalId,
+                    '42': udfAcquirer && udfAcquirer.identificationCode,
+                    '43': udfAcquirer && udfAcquirer.terminalName,
+                    '49': currency.numeric(transferCurrency),
+                    '52': pinBlock,
+                    '102': sourceAccount,
+                    '103': destinationAccount,
+                    '122': merchantId,
+                    '123': merchantInvoice
+                };
+            }
         }
+
+        var result = base();
+        result.mtid = '' + version + udfAcquirer.mti;
+        switch (udfAcquirer.mti) {
+            case 200:
+                break;
+            case 210:
+                break;
+            case 420:
+                result['90'] = '';
+                break;
+            case 430:
+                break;
+            case 800:
+                break;
+            case 810:
+                break;
+            default:
+                result['39'] = ['96', '909'][version];
+        }
+        return result;
     },
     fromISO: function(msg, $meta) {
         if ($meta.mtid === 'error') {
@@ -63,7 +86,40 @@ module.exports = {
                 default: throw errors.genericDecline();
             }
         }
+
+        function base() {
+            switch (msg.mtid.substr(0, 1)) {
+                case '0': return {
+                    amount: msg[4],
+                    destinationSettlementDate: msg[15],
+                    localdateTime: msg[13] + msg[12],
+                    transferIdAcquirer: msg[37],
+                    transferIdIssuer: msg[38],
+                    currency: currency.alphabetic(msg[49]),
+                    pinBlock: msg[52],
+                    sourceAccount: msg[102],
+                    destinationAccount: msg[103],
+                    merchantId: msg[122],
+                    merchantInvoice: msg[123],
+                    udfAcquirer: {
+                        card: msg[2],
+                        processingCode: msg[3],
+                        merchantType: msg[18],
+                        institutionCode: msg[32],
+                        track2: msg[35],
+                        terminalId: msg[41],
+                        identificationCode: msg[42],
+                        terminalName: msg[43]
+                    }
+                };
+                case '1': return {
+
+                };
+            }
+        }
         switch (msg.mtid) {
+            case '0200':
+                return base();
             case '0210':
                 return {
                     balance: {
@@ -72,6 +128,21 @@ module.exports = {
                     },
                     transferIdIssuer: msg[38]
                 };
+            case '0420':
+                return base();
+            case '0430':
+                return base();
+            case '0800':
+                return {
+                    networkCode: msg[70],
+                    networkKey: msg[48]
+                };
+            case '0810':
+                return {
+                    networkCode: msg[70]
+                };
+            default:
+                throw errors.unknown();
         }
     }
 };
