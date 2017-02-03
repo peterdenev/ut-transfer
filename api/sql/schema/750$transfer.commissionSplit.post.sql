ca5de4c6663af25afd1e5dfbdb0cb5f2967fd9a2
krasimir.kostadinov <krasimir.kostadinov@softwaregroup-bg.com>
2017-02-03 20:21:40 +0200

    fix: commissionSplit fix
    Signed-off-by: krasimir.kostadinov <krasimir.kostadinov@softwaregroup-bg.com>

--- api/sql/schema/750$transfer.commissionSplit.post.sql
+++ api/sql/schema/750$transfer.commissionSplit.post.sql
@@ -5,21 +5,21 @@ AS
 
 DECLARE @callParams XML
 DECLARE @userId BIGINT = (SELECT [auth.actorId] FROM @meta)
-    
-BEGIN TRY    
+
+BEGIN TRY
     DECLARE @actionID varchar(100) =  OBJECT_SCHEMA_NAME(@@PROCID) + '.' +  OBJECT_NAME(@@PROCID), @return int = 0
-    EXEC @return = [user].[permission.check] @actionId =  @actionID, @objectId = null, @meta = @meta  
-    
+    EXEC @return = [user].[permission.check] @actionId =  @actionID, @objectId = null, @meta = @meta
+
     IF @return != 0
     BEGIN
         RETURN 55555
     END
-    
-    DECLARE @transferTypeId BIGINT = 
-    ( 
-        SELECT i.itemNameId 
+
+    DECLARE @transferTypeId BIGINT =
+    (
+        SELECT i.itemNameId
         FROM core.itemName i
-        JOIN core.itemType t on t.itemTypeId = i.itemTypeId AND t.alias='operation' 
+        JOIN core.itemType t on t.itemTypeId = i.itemTypeId AND t.alias='operation'
         WHERE i.itemCode = 'commission'
     )
 
@@ -32,7 +32,7 @@ BEGIN TRY
           destinationAccount VARCHAR(50),
           transferAmount MONEY
           )
-      
+
     BEGIN TRANSACTION
     BEGIN
         DECLARE @tranferDT DATETIME = getdate()
@@ -40,16 +40,16 @@ BEGIN TRY
         DECLARE @settlementDate DATE = CONVERT(DATE, @tranferDT)
         DECLARE @today DATETIMEOFFSET = SYSDATETIMEOFFSET()
 
-        
+
 
         -- 1 -->authorized
-        
-        INSERT INTO [transfer].[transfer] (sourceAccount, destinationAccount, transferCurrency, transferAmount, 
-            channelId, channelType, transferTypeId, transferDateTime, localDateTime, settlementDate, reversed, issuerTxState, 
+
+        INSERT INTO [transfer].[transfer] (sourceAccount, destinationAccount, transferCurrency, transferAmount,
+            channelId, channelType, transferTypeId, transferDateTime, localDateTime, settlementDate, reversed, issuerTxState,
             destinationPort, [acquirerFee], [issuerFee], [transferFee], [description])
         OUTPUT inserted.transferId, inserted.sourceAccount, inserted.destinationAccount, inserted.transferAmount INTO #transfer
-        SELECT s.credit, s.debit, 'GHS' /*transferCurrency*/,  sum(s.amount), 
-            s.actorId, 'agent', @transferTypeId, @tranferDT, @localDT, @settlementDate, 0, 1,
+        SELECT s.credit, s.debit, 'GHS' /*transferCurrency*/,  sum(s.amount),
+            s.actorId, 'agent', @transferTypeId, @tranferDT, @localDT, @settlementDate, 0, NULL,
             'cbs',  0.00, 0.00, 0.00, 'COMISSION'
         FROM [transfer].split s
         JOIN @splitIds si ON si.value = s.splitId
@@ -61,14 +61,14 @@ BEGIN TRY
         FROM [transfer].split s
         JOIN @splitIds si ON si.value = s.splitId
         JOIN #transfer t ON t.sourceAccount = s.credit AND t.destinationAccount = s.debit
-       
+
         INSERT INTO [transfer].[splitAudit] (splitId, field, oldValue, createdBy, createdOn)
         SELECT s.splitId, 'state', s.[state], @userId, @today
         FROM [transfer].split s
-        JOIN @splitIds si on si.value = s.splitId     
-           
+        JOIN @splitIds si on si.value = s.splitId
+
     END
-    
+
     COMMIT TRANSACTION
 
     SELECT 'postCommission' AS resultSetName
