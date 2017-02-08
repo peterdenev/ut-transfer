007fd496cdbb0a813e1e444aef134b2f94e5a25b
mtchaparova <maria.tchaparova@softwaregroup-bg.com>
2017-02-08 18:57:54 +0200

    feat: new report add

---
+++ api/sql/schema/750$transfer.commissionSplitPosted.fetch.sql
@@ -0,0 +1,23 @@
+ALTER PROCEDURE [transfer].[commissionSplitPosted.fetch]-- fetch commission posted for actorId
+    @actorId BIGINT,-- actorId of the agent
+	@meta core.metaDataTT READONLY -- information for the user that makes the operation
+AS
+
+SET NOCOUNT ON
+    DECLARE @userId BIGINT = (SELECT [auth.actorId] FROM @meta)
+    DECLARE @actionID VARCHAR(100) =  OBJECT_SCHEMA_NAME(@@PROCID) + '.' +  OBJECT_NAME(@@PROCID), @return INT = 0
+    EXEC @return = [user].[permission.check] @actionId =  @actionID, @objectId = NULL, @meta = @meta
+    IF @return != 0
+ 	BEGIN
+         RETURN 55555
+    END
+
+    SELECT 'commission' as resultSetName
+    SELECT s.splitId, t.transferIdIssuer, t.transferDateTime, i.itemName AS [operation], 
+        s.amount AS commission, t.transferCurrency, t.transferAmount AS transferAmount
+    FROM [transfer].split s
+        JOIN [transfer].[transfer] t ON t.transferId = s.txtId 
+        JOIN core.itemName i ON i.itemNameId = t.transferTypeId 
+    WHERE s.ActorId  = @actorID AND t.channelType ='agent'
+    AND s.tag LIKE '%|commission|%'
+    ORDER BY i.itemName
