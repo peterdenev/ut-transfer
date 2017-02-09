6302d6cafd3e85f42fddac23e2e5954d9e8feeac
mtchaparova <maria.tchaparova@softwaregroup-bg.com>
2017-02-09 18:32:19 +0200

    feat: add new column firstName, lastName

--- api/sql/schema/750$transfer.splitAudit.fetch.sql
+++ api/sql/schema/750$transfer.splitAudit.fetch.sql
@@ -3,6 +3,7 @@ ALTER PROCEDURE [transfer].[splitAudit.fetch] -- fetch splitAudit for splitId
 	@meta core.metaDataTT READONLY -- information for the user that makes the operation
 AS
     SELECT 'splitAudit' as resultSetName
-    SELECT *
-    FROM transfer.splitAudit
+    SELECT s.*, p.firstName, p.lastName
+    FROM transfer.splitAudit s
+    JOIN customer.person p ON p.actorId = s.createdBy
     WHERE splitId= @splitId
