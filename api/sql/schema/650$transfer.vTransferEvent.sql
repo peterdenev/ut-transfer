9eeb96365ec716807fbc8fd8bf46dfa7c35ed9d9
Kalin Krustev <kalin.krustev@softwaregroup-bg.com>
2017-02-24 12:28:27 +1100

    fix: add columns to vTransferEvent

--- api/sql/schema/650$transfer.vTransferEvent.sql
+++ api/sql/schema/650$transfer.vTransferEvent.sql
@@ -38,7 +38,33 @@ SELECT
     cashAlert.eventDateTime [cashAlertDateTime],
     cashAlert.type [cashAlertType],
     cashAlert.message [cashAlertMessage],
-    n.itemName [transferType]
+    (CASE t.[issuerTxState]
+        WHEN 1 THEN 'requested'
+        WHEN 2 THEN 'confirmed'
+        WHEN 3 THEN 'denied'
+        WHEN 4 THEN 'timed out'
+        WHEN 5 THEN 'aborted'
+        WHEN 6 THEN 'error'
+        WHEN 7 THEN 'store requested'
+        WHEN 8 THEN 'store confirmed'
+        WHEN 9 THEN 'store timed out'
+        WHEN 11 THEN 'forward requested'
+        WHEN 12 THEN 'forward confirmed'
+        WHEN 13 THEN 'forward denied'
+        WHEN 14 THEN 'forward timed out'
+        ELSE ''
+    END) [issuerTxStateName],
+    n.itemName [transferType],
+    (CASE
+        WHEN t.[reversed] = 1 THEN 'transferReversed'
+        WHEN t.[issuerTxState] in (2, 8, 12) AND ISNULL(cardAlert.type, cashAlert.type) IS NOT NULL THEN 'transferAlert'
+        WHEN t.[acquirerTxState] in (2, 8, 12) THEN 'transferNormal'
+        ELSE 'transferError'
+    END) [style],
+    CASE
+        WHEN ISNULL(cashAlert.message, '') != '' AND ISNULL(cardAlert.message, '') != '' THEN cashAlert.message + CHAR(10) + CHAR(13) + cardAlert.message
+        ELSE ISNULL(cashAlert.message, '') + ISNULL(cardAlert.message, '')
+    END as alerts
 FROM
     [transfer].[vTransfer] t
 OUTER APPLY
@@ -73,14 +99,14 @@ OUTER APPLY
     (
         SELECT TOP 1 udfDetails, transferId, [type], [message], eventDateTime
         FROM [transfer].[event]
-        WHERE [state] = 'alert' AND [type] = 'atm.cardReaderFault' AND t.transferId = transferId
+        WHERE [state] = 'fail' AND [type] = 'atm.cardReaderFault' AND t.transferId = transferId
         ORDER BY transferId ASC
     ) cardAlert
 OUTER APPLY
     (
         SELECT TOP 1 udfDetails, transferId, [type], [message], eventDateTime
         FROM [transfer].[event]
-        WHERE [state] = 'alert' AND [type] = 'atm.cashHandlerFault' AND t.transferId = transferId
+        WHERE [state] = 'fail' AND [type] = 'atm.cashHandlerFault' AND t.transferId = transferId
         ORDER BY transferId ASC
     ) cashAlert
 INNER JOIN
