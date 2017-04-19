35b11ff5912ebb4398a0d7ee48b1e0b9cbed6d79
kalinkrustev <kalin.krustev@softwaregroup-bg.com>
2017-04-19 11:21:16 +0300

    fix: typo

--- validations/db/transfer/index.js
+++ validations/db/transfer/index.js
@@ -3,5 +3,5 @@ module.exports = {
     'report.byTypeOfTransfer': require('./report.byTypeOfTransfer'),
     'report.byHourOfDay': require('./report.byHourOfDay'),
     'report.byDayOfWeek': require('./report.byDayOfWeek'),
-    'report.byWeekОfYear': require('./report.byWeekОfYear')
+    'report.byWeekOfYear': require('./report.byWeekОfYear')
 };
