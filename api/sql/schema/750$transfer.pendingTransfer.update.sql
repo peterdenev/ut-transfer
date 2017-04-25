dac518a5be8aef13077b44ec97b9609dceffc418
mtchaparova <maria.tchaparova@softwaregroup-bg.com>
2017-04-25 18:59:43 +0300

    feat: some changes

--- api/sql/schema/750$transfer.pendingTransfer.update.sql
+++ api/sql/schema/750$transfer.pendingTransfer.update.sql
@@ -5,8 +5,10 @@ ALTER PROCEDURE [transfer].[pendingTransfer.update] -- update status of pending 
     @meta core.metaDataTT READONLY -- information for the user that makes the operation
 AS
 SET NOCOUNT ON
+DECLARE @callParams XML
+DECLARE @userId BIGINT = (SELECT [auth.actorId] FROM @meta)
 
-    DECLARE @userId BIGINT = (SELECT [auth.actorId] FROM @meta)
+BEGIN TRY
     DECLARE @actionID VARCHAR(100) =  OBJECT_SCHEMA_NAME(@@PROCID) + '.' +  OBJECT_NAME(@@PROCID), @return INT = 0
     EXEC @return = [user].[permission.check] @actionId =  @actionID, @objectId = NULL, @meta = @meta
     IF @return != 0
@@ -22,4 +24,24 @@ SET NOCOUNT ON
         [status] = [status]
     WHERE
         pendingId = @pendingId
+
+    IF @@ROWCOUNT = 1 
+    BEGIN
+        SELECT 'transferPending' AS resultSetName
+        SELECT 'Successfuly'
+
+    END
+    ELSE
+    BEGIN
+        SELECT 'transferPending' AS resultSetName
+        SELECT 'Failed'
+    END
+
+    EXEC core.auditCall @procid = @@PROCID, @params = @callParams
+END TRY
+BEGIN CATCH
+    IF @@TRANCOUNT > 0 ROLLBACK
+	EXEC [core].[error]
+    RETURN 55555
+END CATCH
 
