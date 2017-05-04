f4dbabf63b1d7eae7e0fff0aa4bf76274b4b3db9
mtchaparova <maria.tchaparova@softwaregroup-bg.com>
2017-05-04 17:55:12 +0300

    fix: remove meta

--- api/sql/schema/750$transfer.pendingTransferExpired.fetch.sql
+++ api/sql/schema/750$transfer.pendingTransferExpired.fetch.sql
@@ -1,15 +1,7 @@
 CREATE PROCEDURE [transfer].[pendingTransferExpired.fetch]-- fetch expired initiated pendingTransfer 
-    @meta core.metaDataTT READONLY -- information for the user that makes the operation
 AS
 
 SET NOCOUNT ON
-    DECLARE @userId BIGINT = (SELECT [auth.actorId] FROM @meta)
-    DECLARE @actionID VARCHAR(100) =  OBJECT_SCHEMA_NAME(@@PROCID) + '.' +  OBJECT_NAME(@@PROCID), @return INT = 0
-    EXEC @return = [user].[permission.check] @actionId =  @actionID, @objectId = NULL, @meta = @meta
-    IF @return != 0
-    BEGIN
-        RETURN 55555
-    END
 
     SELECT 'pendingTransferExpired' as resultSetName
 
