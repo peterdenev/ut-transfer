2abd1272165888f3fbd04d99037a15e849e95c0c
SOFTWARE-GROUP\Yordan.Terziev <yordan.terziev@softwaregroup-bg.com>
2017-09-21 10:24:08 +0300

    fix: FID-870 - Audit Commissions reports: Spelling mistake

--- api/sql/schema/750$transfer.splitAudit.fetch.sql
+++ api/sql/schema/750$transfer.splitAudit.fetch.sql
@@ -3,14 +3,14 @@ ALTER PROCEDURE [transfer].[splitAudit.fetch] -- fetch splitAudit for splitId
 	@meta core.metaDataTT READONLY -- information for the user that makes the operation
 AS
     SELECT 'splitAudit' as resultSetName
-    SELECT s.id, s.splitId, s.field, 
-    CASE 
-        WHEN s.field ='status' AND s.oldValue IS NULL THEN 'Pending Autorization'
-        WHEN s.field ='status' AND s.oldValue = '2' THEN  'Posted' 
-        WHEN s.field ='status' AND s.oldValue = '5' THEN  'Rejected' 
-        WHEN s.field ='status' AND s.oldValue = '4' THEN  'Authorized' 
+    SELECT s.id, s.splitId, s.field,
+    CASE
+        WHEN s.field ='status' AND s.oldValue IS NULL THEN 'Pending Authorization'
+        WHEN s.field ='status' AND s.oldValue = '2' THEN  'Posted'
+        WHEN s.field ='status' AND s.oldValue = '5' THEN  'Rejected'
+        WHEN s.field ='status' AND s.oldValue = '4' THEN  'Authorized'
         ELSE s.oldValue
-    END AS oldValue, 
+    END AS oldValue,
     s.createdOn,
     p.firstName, p.lastName
     FROM transfer.splitAudit s
