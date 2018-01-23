5528569e38a00bb714fb604f32178b4f70ce6e85
Name <d0ntth1nc@gmail.com>
2018-01-23 22:56:02 +0200

    fix: reverse transactions with requested acquirer state (#143)

--- api/sql/schema/650$transfer.vReversal.sql
+++ api/sql/schema/650$transfer.vReversal.sql
@@ -5,8 +5,8 @@ FROM
     [transfer].[transfer] t
 JOIN
     [transfer].[partner] p ON (((
-        (t.issuerTxState = 2 AND ISNULL(t.acquirerTxState, 0) IN (0 , 3, 4, 5) AND p.mode = 'online' AND t.channelType IN ('ATM')) OR --tx succeeded at issuer during online but failed at acquirer and current mode is online
-        (t.issuerTxState = 8 AND ISNULL(t.acquirerTxState, 0) IN (0 , 3, 4, 5) AND p.mode IN ('online', 'offline')) OR --tx succeeded at issuer during offline but failed at acquirer and current mode is online/offline
+        (t.issuerTxState = 2 AND ISNULL(t.acquirerTxState, 0) IN (0, 1, 3, 4, 5) AND p.mode = 'online' AND t.channelType IN ('ATM')) OR --tx succeeded at issuer during online but failed at acquirer and current mode is online
+        (t.issuerTxState = 8 AND ISNULL(t.acquirerTxState, 0) IN (0, 1, 3, 4, 5) AND p.mode IN ('online', 'offline')) OR --tx succeeded at issuer during offline but failed at acquirer and current mode is online/offline
         (ISNULL(t.merchantTxState, 0) IN (1, 3)) OR                                                               --tx failed at merchant (succeeded at issuer implicitly)
         (t.issuerTxState IN (1,4) AND p.mode = 'online') OR                                                    --tx timed out at issuer during online and current mode is online
         (t.issuerTxState IN (7,9) AND p.mode IN ('online', 'offline'))                                        --tx timed out during offline and current mode is online/offline
