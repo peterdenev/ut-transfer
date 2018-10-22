c00cd52966764062328ec64b87ca965c16801b36
pavel.georgiev <pavel.georgiev@softwaregroup-bg.com>
2018-10-22 17:28:07 +0300

    feat: deploy logic for transfer type and split description

--- api/sql/schema/750$transfer.transfer.updateInternalAccountsAndTransfers.sql
+++ api/sql/schema/750$transfer.transfer.updateInternalAccountsAndTransfers.sql
@@ -5,10 +5,30 @@ AS
 BEGIN TRY
     DECLARE @tranCounter INT = @@tranCount
     IF @tranCounter = 0
+	DECLARE @splitId [core].[arrayNumberList]
+	INSERT INTO @splitId
+	SELECT s.splitId 
+	FROM [transfer].[transfer] t
+		  JOIN @transfersId ti on t.transferId=ti.value 
+		  JOIN [transfer].[split] s ON s.transferId = t.transferId
+		  JOIN (SELECT s.transferTypeId,s.splitDescription FROM [settlement].[transferTypeSplitTagMapping] s
+				UNION
+				SELECT i.itemNameId,'Fee' from core.itemName i where i.itemTypeId=1 AND itemCode like 'walletToVendor%'
+				UNION
+				SELECT i.itemNameId,'VAT' from core.itemName i where i.itemTypeId=1 AND itemCode like 'walletToVendor%'
+				UNION
+				SELECT i.itemNameId,'other tax' from core.itemName i where i.itemTypeId=1 AND itemCode like 'walletToVendor%'
+				UNION
+				SELECT i.itemNameId,'Vendor fee' from core.itemName i where i.itemTypeId=1 AND itemCode like 'walletToVendor%'
+			     )ttsm ON ttsm.transferTypeId=t.transferTypeId AND s.description=ttsm.splitDescription
+
+
     BEGIN TRANSACTION
 	   UPDATE  t SET settlementDate=@settlementDate 
-	   FROM[transfer].[transfer] t
-	   JOIN @transfersId ti on t.transferId=ti.value 
+	    FROM [transfer].[transfer] t		
+		  JOIN(SELECT DISTINCT s.transferId 
+			 FROM [transfer].[split] s 
+			 JOIN @splitId st on st.value=s.splitId)s ON s.transferId = t.transferId
 
 	   UPDATE b 
 	   SET debit=b.debit+r.debit
@@ -17,9 +37,9 @@ BEGIN TRY
 	   JOIN(
 		  SELECT SUM(s.amount) debit
 		  ,a.accountId
-		  FROM [transfer].[transfer] t
-		  JOIN @transfersId ti on t.transferId=ti.value 
+		  FROM [transfer].[transfer] t		
 		  JOIN [transfer].[split] s ON s.transferId = t.transferId
+		  JOIN @splitId st on st.value=s.splitId
 		  JOIN ledger.account a ON a.accountNumber = s.debit AND isInternal = 1 AND isForSettlement = 1
 		  GROUP BY a.accountId
 	   )r ON a.accountId=r.accountId
@@ -31,9 +51,9 @@ BEGIN TRY
 	   JOIN(
 		  SELECT SUM(s.amount) credit
 		  ,a.accountId
-		  FROM [transfer].[transfer] t
-		  JOIN @transfersId ti on t.transferId=ti.value 
+		 FROM [transfer].[transfer] t		
 		  JOIN [transfer].[split] s ON s.transferId = t.transferId
+		  JOIN @splitId st on st.value=s.splitId
 		  JOIN ledger.account a ON a.accountNumber = s.credit AND isInternal = 1 AND isForSettlement = 1
 		  GROUP BY a.accountId
 	   )r ON a.accountId=r.accountId
