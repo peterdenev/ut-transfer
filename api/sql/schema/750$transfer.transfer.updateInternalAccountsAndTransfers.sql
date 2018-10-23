1a2c7829b372032a05e43bbf7c141927eef8376b
pavel.georgiev <pavel.georgiev@softwaregroup-bg.com>
2018-10-23 11:50:53 +0300

    feat: revert amount sign commit

--- api/sql/schema/750$transfer.transfer.updateInternalAccountsAndTransfers.sql
+++ api/sql/schema/750$transfer.transfer.updateInternalAccountsAndTransfers.sql
@@ -5,25 +5,21 @@ AS
 BEGIN TRY
     DECLARE @tranCounter INT = @@tranCount
     IF @tranCounter = 0
-	IF OBJECT_ID('tempdb..#split') IS NOT NULL
-    DROP TABLE #split
-
-CREATE TABLE #split (splitId BIGINT,amountSign smallint)
-	 
-	INSERT INTO #split
+	DECLARE @splitId [core].[arrayNumberList]
+	INSERT INTO @splitId
 	SELECT s.splitId 
 	FROM [transfer].[transfer] t
 		  JOIN @transfersId ti on t.transferId=ti.value 
 		  JOIN [transfer].[split] s ON s.transferId = t.transferId
-		  JOIN (SELECT s.transferTypeId,s.splitDescription,s.amountSign FROM [settlement].[transferTypeSplitTagMapping] s
+		  JOIN (SELECT s.transferTypeId,s.splitDescription FROM [settlement].[transferTypeSplitTagMapping] s
 				UNION
-				SELECT i.itemNameId,'Fee',-1 amountSign from core.itemName i where i.itemTypeId=1 AND itemCode like 'walletToVendor%'
+				SELECT i.itemNameId,'Fee' from core.itemName i where i.itemTypeId=1 AND itemCode like 'walletToVendor%'
 				UNION
-				SELECT i.itemNameId,'VAT',-1 amountSign from core.itemName i where i.itemTypeId=1 AND itemCode like 'walletToVendor%'
+				SELECT i.itemNameId,'VAT' from core.itemName i where i.itemTypeId=1 AND itemCode like 'walletToVendor%'
 				UNION
-				SELECT i.itemNameId,'other tax',-1 amountSign from core.itemName i where i.itemTypeId=1 AND itemCode like 'walletToVendor%'
+				SELECT i.itemNameId,'other tax' from core.itemName i where i.itemTypeId=1 AND itemCode like 'walletToVendor%'
 				UNION
-				SELECT i.itemNameId,'Vendor fee',-1 amountSign from core.itemName i where i.itemTypeId=1 AND itemCode like 'walletToVendor%'
+				SELECT i.itemNameId,'Vendor fee' from core.itemName i where i.itemTypeId=1 AND itemCode like 'walletToVendor%'
 			     )ttsm ON ttsm.transferTypeId=t.transferTypeId AND s.description=ttsm.splitDescription
 
 
@@ -32,18 +28,18 @@ CREATE TABLE #split (splitId BIGINT,amountSign smallint)
 	    FROM [transfer].[transfer] t		
 		  JOIN(SELECT DISTINCT s.transferId 
 			 FROM [transfer].[split] s 
-			 JOIN #split st on st.splitId=s.splitId)s ON s.transferId = t.transferId
+			 JOIN @splitId st on st.value=s.splitId)s ON s.transferId = t.transferId
 
 	   UPDATE b 
 	   SET debit=b.debit+r.debit
 	   FROM ledger.balance b 
 	   JOIN ledger.account a on b.accountId=a.accountId
 	   JOIN(
-		  SELECT SUM(s.amount*st.amountSign) debit
+		  SELECT SUM(s.amount) debit
 		  ,a.accountId
 		  FROM [transfer].[transfer] t		
 		  JOIN [transfer].[split] s ON s.transferId = t.transferId
-		  JOIN #split st on st.splitId=s.splitId
+		  JOIN @splitId st on st.value=s.splitId
 		  JOIN ledger.account a ON a.accountNumber = s.debit AND isInternal = 1 AND isForSettlement = 1
 		  GROUP BY a.accountId
 	   )r ON a.accountId=r.accountId
@@ -53,11 +49,11 @@ CREATE TABLE #split (splitId BIGINT,amountSign smallint)
 	   FROM ledger.balance b 
 	   JOIN ledger.account a on b.accountId=a.accountId
 	   JOIN(
-		    SELECT SUM(s.amount*st.amountSign) debit
+		  SELECT SUM(s.amount) credit
 		  ,a.accountId
-		  FROM [transfer].[transfer] t		
+		 FROM [transfer].[transfer] t		
 		  JOIN [transfer].[split] s ON s.transferId = t.transferId
-		  JOIN #split st on st.splitId=s.splitId
+		  JOIN @splitId st on st.value=s.splitId
 		  JOIN ledger.account a ON a.accountNumber = s.credit AND isInternal = 1 AND isForSettlement = 1
 		  GROUP BY a.accountId
 	   )r ON a.accountId=r.accountId
