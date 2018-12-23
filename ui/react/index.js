b307a97ebb0737c85a2b75438ee2058b4177f3f7
Kalin Krustev <kalin.krustev@gmail.com>
2018-12-23 14:49:42 +0200

    fix: react-router upgrade & hot reload

--- ui/react/index.js
+++ ui/react/index.js
@@ -1,17 +1,10 @@
 import React from 'react';
-import {Route} from 'react-router';
-import { getRoute } from 'ut-front-react/routerHelper';
-import registerRoutes from './registerRoutes';
-import { Partner } from './pages';
 import reducer from './reducers';
-
-registerRoutes();
+import TransferRoutes from './routes';
 
 export function ui() {
     return {
         reducer: () => reducer,
-        route: async() => <Route key='utTransfer' path={getRoute('ut-transfer:home')}>
-            <Route path={getRoute('ut-transfer:partners')} component={Partner} />
-        </Route>
+        route: async() => <TransferRoutes key='utTransfer' />
     };
 };
