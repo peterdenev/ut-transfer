633467744412360aa227b8eef5816799db59601b
Georgi Kocev <georgi.kocev@softwaregroup-bg.com>
2019-01-17 14:21:59 +0200

    fix: bank name type

--- api/sql/schema/350$transfer.payee.sql
+++ api/sql/schema/350$transfer.payee.sql
@@ -4,7 +4,7 @@ CREATE TABLE [transfer].[payee](
     payeeName NVARCHAR(100) NOT NULL,
     accountTypeId BIGINT NOT NULL,
     accountNumber VARCHAR(50) NOT NULL,
-    bankName VARCHAR(100) NOT NULL,
+    bankName NVARCHAR(100) NOT NULL,
     SWIFT VARCHAR(11) NOT NULL,
     isDeleted BIT NOT NULL DEFAULT(0),
     CONSTRAINT [pkTransferPayee] PRIMARY KEY CLUSTERED (payeeId ASC),
